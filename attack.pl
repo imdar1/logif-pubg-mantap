959e96af7663887df11e5ac1967c50c767888704
rafizhafraan <44691344+rafizhafraan@users.noreply.github.com>
2018-11-23 21:44:53 +0700

    Update attack.pl

--- attack.pl
+++ attack.pl
@@ -1,7 +1,8 @@
 attack  :- enemy(Xe,Ye,Weapone), player(Xp,Yp,Healthp,Armorp,Weaponp,Ammop),
-        Xp =:= Xe, Yp tr =:= Ye, !, weapon(X,Y,Weapone,Damagee), Armorp > 0, !, 
-        Armornp is Armorp - Damagee, Armornp < 0, !, 
-        Healthnp is Healthp + Armornp, Armornp is 0, Ammonp is Ammop - 1,
+        Xp =:= Xe, Yp tr =:= Ye, !, weapon(X,Y,Weapone,Damagee), 
+        Ammop > 0, !, Ammonp is Ammop - 1,
+        Armorp > 0, !, Armornp is Armorp - Damagee, Armornp < 0, !, 
+        Healthnp is Healthp + Armornp, Armornp is 0,
         retract(player(Xp,Yp,Healthp,Armorp,Weaponp,Ammop)), Healthnp > 0, !,
         assertz(player(Xp,Yp,Healthnp,Armornp,Weaponp,Ammonp)),
         retract(enemy(Xe,Ye,Weapone)), Xne is 1, Yne is 1, 
@@ -11,9 +12,11 @@ attack  :- enemy(Xe,Ye,Weapone), player(Xp,Yp,Healthp,Armorp,Weaponp,Ammop),
         write(" serangan. Untungnya serangan itu dapat dikurangi dengan pelindung."),
         write(" Kamu telah membunuh musuhmu dan musuhmu meletakkan beberapa benda.").
 
-attack  :- Xp != Xe, Yp != Ye, write("Tidak ada musuh di sekelilingmu.").
+attack  :- Xp =/= Xe, Yp =/= Ye, write("Tidak ada musuh di sekelilingmu."), nl.
 
-attack  :- Armorp < 0, Healthnp is Healthp - Damagee, Ammonp is Ammop - 1, 
+attack  :- Ammop =:= 0, write("Pelurumu habis! Silahkan cari peluru dulu."), nl.
+
+attack  :- Armorp =:= 0, Healthnp is Healthp - Damagee,
         retract(player(Xp,Yp,Healthp,Armorp,Weaponp,Ammop)), Healthnp > 0, !, 
         assertz(player(Xp,Yp,Healthnp,Armornp,Weaponp,Ammonp)),
         retract(enemy(Xe,Ye,Weapone)), Xne is 1, Yne is 1, 
@@ -23,7 +26,7 @@ attack  :- Armorp < 0, Healthnp is Healthp - Damagee, Ammonp is Ammop - 1,
         write(" serangan."),
         write(" Kamu telah membunuh musuhmu dan musuhmu meletakkan beberapa benda.").
 
-attack  :- Armornp > 0, Ammonp is Ammop - 1,
+attack  :- Armornp > 0,
         retract(player(Xp,Yp,Healthp,Armorp,Weaponp,Ammop)),
         assertz(player(Xp,Yp,Healthp,Armornp,Weaponp,Ammonp)),
         retract(enemy(Xe,Ye,Weapone)), Xne is 1, Yne is 1, 
@@ -33,7 +36,7 @@ attack  :- Armornp > 0, Ammonp is Ammop - 1,
         write(" serangan. Untungnya serangan itu dapat dikurangi dengan pelindung."),
         write(" Kamu telah membunuh musuhmu dan musuhmu meletakkan beberapa benda.").
 
-attack  :- Healthnp < 0, Healthnp is 0, Ammonp is Ammop - 1,
+attack  :- Healthnp <= 0, Healthnp is 0, Ammonp is Ammop - 1,
         retract(player(Xp,Yp,Healthp,Armorp,Weaponp,Ammop)),
         assertz(player(Xp,Yp,Healthp,Armornp,Weaponp,Ammonp)),
         write("Kamu telah mati.").
